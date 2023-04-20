743cd8c436079706af67e657d43d7a4f1302e2a8
佐々木壮馬 <soma@MacBook-Air.local>
2023-04-20 22:20:15 +0900

    topページとuserのroutes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    authenticate :user do
+      get 'users/profile/edit', to: 'users/registrations#profile_edit', as: 'user_profile_edit'
+      get 'users/profile', to: 'users/registrations#profile', as: 'user_profile'
+      get 'users/account', to: 'users/registrations#account', as: 'user_account'
+    end
+  end
+
+  get "/",to:'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
