1ec82118f3dc1694b45287e58d22f7590c7fc758
佐々木壮馬 <soma@MacBook-Air.local>
2023-04-20 22:14:47 +0900

    user deviseの実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,14 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # ホストの設定
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+  Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -72,5 +80,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  # config.action_casle.disable_request_forgery_protection = true
+end
 end
