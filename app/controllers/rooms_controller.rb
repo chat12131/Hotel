0fc0b4ae1ab3895a2cc1ad344e2ac7306c0138d0
佐々木壮馬 <soma@MacBook-Air.local>
2023-06-05 00:18:49 +0900

    room機能を実装

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,82 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:edit, :update, :destroy,]
+  before_action :authorize_user, only: [:edit, :update, :destroy, ]
+  before_action :authenticate_user!, only: [:myrooms, :new]
+
+
+
+  def myrooms
+    if current_user
+      @rooms = current_user.rooms
+    else
+      flash[:notice] = "権限がありません"
+      redirect_to "/"
+    end
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user = current_user
+    if @room.save
+      if @room.image_name.present?
+        @room.image_name = "#{@room.id}.jpg"
+        @room.save
+      end
+      redirect_to "/"
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to room_path(@room)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    flash[:notice] = "削除しました"
+    redirect_to myrooms_path
+  end
+
+  def search
+    @rooms = Room.search(params[:address_keyword], params[:details_keyword])
+    @address_keyword = params[:address_keyword]
+    @details_keyword = params[:details_keyword]
+    render "index"
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def authorize_user
+    unless current_user == @room.user
+      flash[:notice] = "権限がありません"
+      redirect_to "/"
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :address, :price, :details, :image_name, :user_id)
+  end
+end
---
+++ app/models/room.rb
@@ -0,0 +1,14 @@
+class Room < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :details, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+
+  mount_uploader :image_name, RoomImageUploader
+
+  def self.search(address_keyword, details_keyword)
+    where(["address like?", "%#{address_keyword}%"]).where(["name like? OR details like?", "%#{details_keyword}%", "%#{details_keyword}%"])
+  end
+end
